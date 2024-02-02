3702de65d39e5ca6f9572f8fbaba71a51a0a6130
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-02 15:05:05 +0100

    Create lecture-19.py

---
+++ lecture-19.py
@@ -0,0 +1,11 @@
+#index operator [] = gives access to a sequence elements (str,lists,tuples)
+
+name = "petr Kroutil!"
+
+#if(name[0].islower()):
+    #name = name.capitalize()
+    
+#first_name = name[:4].upper()
+#last_name = name[5:].upper()
+last_character = name[-1]
+print(last_character)
