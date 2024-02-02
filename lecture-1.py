02917440ea1e992316257f4ed05531a0d80381a4
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-02 09:26:40 +0100

    Create lecture-1.py
    first lecture :)

---
+++ lecture-1.py
@@ -0,0 +1,22 @@
+#variables, data types and all the stuffs
+
+#Strings
+first_name = 'Petr'
+last_name = 'Kroutil'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+#Int
+age = 51
+age += 1
+# print("Your age is " + age) type error
+print("Your age is " + str(age)) #simple casting
+
+#float
+height = 250.5
+print(height)
+
+#boolean
+human = True
+print(human)
+print("Are you a human? " + str(human))
