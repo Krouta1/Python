666b437db5773549cb7230605e332697858c5aff
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-02 14:38:46 +0100

    Create lecture-15.py

---
+++ lecture-15.py
@@ -0,0 +1,10 @@
+#2Dlists (list of lists)
+
+drinks = ["coffee","soda","tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner,dessert]
+print(food)
+print(food[0])
+print(food[1][2])
