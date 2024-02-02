c08b5e802f3a84cf8bac131af72b6754d00a406c
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-02 10:30:55 +0100

    Create lecture-6.py

---
+++ lecture-6.py
@@ -0,0 +1,18 @@
+#math in python
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi,2))
+print(math.sqrt(pi))
+print(max(x,y,z))
+
+
+
