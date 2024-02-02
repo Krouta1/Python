d2be13bdf24e7f1a7ab0874cc1a224a3212fec5c
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-02 15:34:12 +0100

    Create lecture-20.py

---
+++ lecture-20.py
@@ -0,0 +1,9 @@
+#functions
+
+def hello(name,age):
+    print("Hello from the function side.")
+    print("Hello " + name + ". You are " + str(age))
+    
+my_name = "Petr"
+my_age = 150
+hello(my_name,my_age)
