a6f367ec1cb9e6157e25154a1b3dbe2c529fb5b4
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-02 14:30:37 +0100

    Create lecture-14.py

---
+++ lecture-14.py
@@ -0,0 +1,15 @@
+#lists
+
+food = ["pizza", "pasta", "hamburger", "hotdog"]
+
+print(food)
+print(food[2])#hamburger :) simple
+
+food[0] = "sushi"
+food.append("ice cream")
+food.remove("pasta")
+food.sort()
+
+for x in food:
+    print(x)
+
