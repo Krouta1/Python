fa79323c72d042ad185caf33fb64cc85efee357c
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-02 10:25:32 +0100

    Create lecture-5.py

---
+++ lecture-5.py
@@ -0,0 +1,9 @@
+#user input
+
+name = input("What is your name?: ")
+print("Hello " + name)
+
+age = int(input("How old are you?:"))
+age +=1
+
+print("Hello " + name +  ". You are " + str(age) + " years old.")
