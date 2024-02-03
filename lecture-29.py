b16012ee9dbafbe40b05c2b49e3c8a5828f5d348
Petr Kroutil <pkroutil1@seznam.cz>
2024-02-03 17:13:55 +0100

    lecture-29

--- lecture-29.py
+++ lecture-29.py
@@ -1,6 +1,26 @@
+import shutil
+import os
 #writing files
 
 with open('test.txt', 'w') as f:
     f.write('Hello, world!\n')
     f.write('How are you?\n')
     f.write('I am fine.\n')
+
+#copying files
+shutil.copy('test.txt', 'test_copy.txt')
+
+#moving files
+source = 'test.txt'
+destination = '/Users/petrkroutil/Downloads/test.txt'
+
+try:
+    if os.path.exists(destination):
+        print(f"File {destination} already exists")
+    elif os.path.exists(source):
+        shutil.move(source, destination)
+except FileNotFoundError:
+    print(f"File {source} not found")
+
+#deleting files
+os.remove('test_copy.txt')
