b1872468e31152fed2f1d62ddf8ba56228b5c13c
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-02-05 19:43:23 +0100

    gui-windows

---
+++ gui-windows.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+window = Tk() # instantiate an instance of a window
+
+window.geometry("500x500") # set the size of the window
+
+window.title("My first GUI window") # set the title of the window
+
+window.configure(background="light blue") # set the background color of the window
+
+window.mainloop() #place the window on the screen
+
+
+
+
