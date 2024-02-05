8b97efdda635cbaa38f5589e0e4a746ed0d7711a
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-05 12:50:11 +0100

    Create lecture-39.py

---
+++ lecture-39.py
@@ -0,0 +1,13 @@
+# functions to a variable
+
+def hello():
+    print("Hello")
+#almost same like in JS 
+hi = hello
+hi()
+hello()
+
+
+say = print
+say("Hello from say")
+print("Hello from print")
