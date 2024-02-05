54cdee38f3ba13acefcfaecb35e84d8f2af30ed7
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-05 15:11:53 +0100

    Create lecture-42.py

---
+++ lecture-42.py
@@ -0,0 +1,15 @@
+# sort method
+
+students = ["Sandy","Petr","Tom","Lucy","Anna"]
+
+students.sort(reverse=True)
+sorted_students = sorted(students,reverse=True)
+
+students_two = [("Sandy","F",60),
+                ("Adam","D",100),
+                ("Lucy","E",24),
+                ("Tom","A",11),
+                ("Zara","B",30)]
+students_two.sort
+for i in sorted_students:
+    print(i)
