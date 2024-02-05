b544b6112d2bb578128a0c0e5a9e7f37e4b644a6
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-05 13:53:19 +0100

    Create lecture-40.py

---
+++ lecture-40.py
@@ -0,0 +1,14 @@
+# higher oder functions
+
+def loud(text):
+    return text.upper()
+
+def quiet(text):
+    return text.lower()
+
+def hello(func):
+    text = func("Hello")
+    print(text)
+
+hello(loud)
+hello(quiet)
