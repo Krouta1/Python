3512771daa6bb106014dc9bb05d09b816a1ed27e
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-02-05 16:46:49 +0100

    lecture-44

--- lecture-44.py
+++ lecture-44.py
@@ -1,3 +1,4 @@
+import functools
 #filter function
 friends = [
     ("Rolf", 25),
@@ -11,3 +12,14 @@ friends_over_30 = list(filter(age, friends))
 
 for i in friends_over_30:
     print(i)
+
+#reduce function
+letters = ['h', 'e', 'l', 'l', 'o']
+word = functools.reduce(lambda a, b: a + b, letters)
+
+print(word)
+
+#this is 5 factorial
+numbers = [1, 2, 3, 4, 5]
+result = functools.reduce(lambda a, b: a * b, numbers)
+print(result)
